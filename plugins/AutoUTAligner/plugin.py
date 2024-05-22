a6069f2a4bfa00706014f257dccaed468c1697eb
topeberti <albertovicentedelegido@gmail.com>
2024-05-22 13:02:50 +0200

    AutoUTAligner finished

--- plugins/AutoUTAligner/plugin.py
+++ plugins/AutoUTAligner/plugin.py
@@ -12,48 +12,70 @@ import plugins.AutoUTAligner.autoutaligner as autoalg
 class Plugin(BasePlugin):
     # Prompt the user to select a file
     def run(self):
-        # Open a file dialog to select a file
-        file_path = self.select_file("Select UT File")
 
-        # Open a file dialog to select a folder to save the rotated volume
-        save_path = self.select_save_file("Select filename to save the aligned volume")
+        batchmode = True
 
-        # Check if a file was selected
-        if file_path and save_path:
-            print("the path is: ", file_path)
+        files_paths = []
+        saves_paths = []
 
-            # Create a progress window
+        while batchmode:
 
-            volume = tiff.imread(file_path)
+            # Open a file dialog to select a file
+            files_paths.append(self.select_file("Select UT File"))
 
-            gate = autoalg.auto_gate(volume)
+            batchmode = self.prompt_confirmation("Do you want to use more files?")
+        
+        for file_path in files_paths:
 
-            print(gate)
+            # Check if a file was selected
+            if file_path:
 
-            volume = alg.align(volume, gate)
+                # Create a progress window
 
-            # align the x,y axis
+                # Load the 3D volume from a .tif file
+                volume = tiff.imread(file_path)
+                
+                # Check if the maximum value in the volume is greater than 128
+                if autoalg.is_RF(volume):
+                    # Apply the Hilbert transform to the volume
+                    amplitude= autoalg.hillbert_transform(volume)
+                    # Find the range around the brightest slice in the amplitude
+                    gate = autoalg.auto_gate(amplitude)
+                    print(gate)
+                    # Align the amplitude and the volume based on the maximum value in the gated range
+                    amplitude, volume = autoalg.double_align(amplitude,volume, gate)
+                    # Crop the center of each slice in the amplitude
+                    cropped = autoalg.crop_volume(amplitude)
+                    # Calculate the rotation angle of the largest component in the cropped amplitude
+                    angle = autoalg.angle_max(cropped)
+                    # Rotate the volume and the amplitude by the calculated angle
+                    rotated_volume = alg.rotate_volume(volume, angle, self)
+                    rotated_amplitude = alg.rotate_volume(amplitude, angle, self)
+                    # Save the rotated volume and amplitude
+                    save_path = file_path.replace(".tif", "_aligned.tif")
+                    tiff.imsave(save_path, rotated_volume)
+                    save_path = save_path.replace("_aligned.tif", "_amplitude.tif")
+                    tiff.imsave(save_path, rotated_amplitude)
+                else:
+                    # Find the range around the brightest slice in the volume
+                    gate = autoalg.auto_gate(volume)
+                    print(gate)
+                    # Align the volume based on the maximum value in the gated range
+                    volume = alg.align(volume, gate)
+                    # Crop the center of each slice in the volume
+                    cropped = autoalg.crop_volume(volume)
+                    # Calculate the rotation angle of the largest component in the cropped volume
+                    angle = autoalg.angle_max(cropped)
+                    # Rotate the volume by the calculated angle
+                    rotated_volume = alg.rotate_volume(volume, angle, self)
+                    # Save the rotated volume
+                    save_path = file_path.replace(".tif", "_aligned.tif")
+                    tiff.imsave(save_path, rotated_volume)
 
-            angle = alg.get_angle(volume, gate)
-
-            # create a progress window
-
-            self.update_progress(0, "Rotating Volume")
-
-            rotated_volume = alg.rotate_volume(volume, angle, self)
-
-            self.update_progress(100, "Rotating Volume", 1, 1)
-
-            # Save the rotated volume
-
-            tiff.imsave(save_path, rotated_volume)
-
-            # Show a message box if the rotated volume is saved successfully, when the message box is closed, close all matplotlib figures
-            self.prompt_message("Aligned volume saved successfully.")
-            self.request_gui(plt.close, "all")
-
-        else:
-            # Show a message box if no file is selected
-            self.prompt_error("No file selected.")
+            else:
+                # Show a message box if no file is selected
+                self.prompt_error("No file selected.")
+        
+        self.prompt_message("All files have been aligned.")
 
     
