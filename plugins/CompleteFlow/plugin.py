f45cd0d5531e0a16132e80c17e963094a3484eee
topeberti <albertovicentedelegido@gmail.com>
2025-05-15 13:27:49 +0200

    New complete flow and onlypores

--- plugins/CompleteFlow/plugin.py
+++ plugins/CompleteFlow/plugin.py
@@ -6,9 +6,9 @@ from utils import image_sequence
 import os
 import numpy as np
 import matplotlib.pyplot as plt
-from plugins.Aligner import aligner as alg
 import plugins.Centering.centering as cnt
-import plugins.Reslicer.reslicer as rsl
+from preprocess_tools import reslicer
+from preprocess_tools import aligner
 import logging
 import tifffile as tiff
 
@@ -53,104 +53,29 @@ class Plugin(BasePlugin):
                 # load the file
                 volume = image_sequence.read_sequence2(file_path, progress_window=self)
 
-            reslices = {"Main": True, "Left": True, "Top": True}
+            #######ALIGNING######
+            self.update_progress(0, "Aligning volume.")
+            volume = aligner.main(volume,False)
+            
+            #reslice and rotate90
 
-            # Select thresholds
-            angles = alg.get_angles_auto(plugin = self, volume=volume, reslices=reslices)
+            volume = reslicer.rotate_auto(volume)
+            volume = reslicer.reslice(volume,'Bottom')
 
-            logging.info("-----------------------")
-
-            # Perform operations on the selected file based on user inputs
-            if angles:
-                if reslices["Main"]:
-                    # main
-                    # Create a progress window
-
-                    self.update_progress(0, "Rotating Volume from Main reslice")
-                    print("The angles: ", angles)
-                    angle = angles["Main"]
-                    volume = alg.rotate_volume_concurrent(
-                        volume, angle, progress_window=self
-                    )
-                    self.update_progress(100, "Rotating Volume from Main reslice")
-
-                if reslices["Left"]:
-                    # Create a progress window
-                    self.update_progress(0, "Rotating Volume from Left reslice")
-                    volume = np.transpose(volume, (2, 0, 1))
-                    angle = angles["Left"]
-                    volume = alg.rotate_volume_concurrent(
-                        volume, angle, progress_window=self
-                    )
-                    self.update_progress(100, "Rotating Volume from Left reslice")
-
-                    if not reslices["Top"]:
-                        volume = np.transpose(volume, (2, 0, 1))
-                        volume = np.transpose(volume, (2, 0, 1))
-
-                if reslices["Top"]:
-                    # Create a progress window
-                    self.update_progress(0, "Rotating Volume from Top reslice")
-                    if not reslices["Left"]:
-                        volume = np.transpose(volume, (2, 0, 1))
-                    volume = np.transpose(volume, (2, 0, 1))
-                    angle = angles["Top"]
-                    volume = alg.rotate_volume_concurrent(
-                        volume, angle, progress_window=self
-                    )
-                    self.update_progress(100, "Rotating Volume from Top reslice")
-
-                    volume = np.transpose(volume, (2, 0, 1))
-
-                #######CENTERING######
-                
-                # Select thresholds
-                user_inputs = cnt.get_thresholds_auto(self, volume)
-
-                # Perform operations on the selected file based on user inputs
-                if len(user_inputs) > 0:
-                    # main
-                    # Create a progress window
-
-                    self.update_progress(0, "Centering Volume")
-                    #get shift
-                    shift = user_inputs['Main']
-                    #shift volume
-                    volume = cnt.shift_volume_concurrent(volume,shift,progress_window=self)
-
-                    #cropping
-                    volume = cnt.crop_volume_auto(volume)
-                
-                #reslice and rotate90
-
-                #ask for reslice direction
-                reslice_direction = "Left"
-
-                #reslice the volume
-                resliced = rsl.reslice(volume, reslice_direction)
-
-                resliced = rsl.rotate_auto(resliced)
-
-                print(resliced.shape)
-
-                # save the resliced volume
-                if file_save:
-                    self.update_progress(0, "Saving resliced volume.")
-                    tiff.imwrite(save_path, resliced)
-                else:
-                    image_sequence.write_sequence2(
-                        save_path,
-                        os.path.basename(save_path),
-                        resliced,
-                        progress_window=self,
-                    )
-                self.update_progress(100, "Resliced volume saved successfully.")
-                self.prompt_message("Resliced volume saved successfully.")
-                self.request_gui(plt.close, "all")
-
+            # save the resliced volume
+            if file_save:
+                self.update_progress(0, "Saving resliced volume.")
+                tiff.imwrite(save_path, volume)
             else:
-                # Show a message box if user inputs are not provided
-                self.prompt_message("User inputs are not provided.")
+                image_sequence.write_sequence2(
+                    save_path,
+                    os.path.basename(save_path),
+                    volume,
+                    progress_window=self,
+                )
+            self.update_progress(100, "Resliced volume saved successfully.")
+            self.prompt_message("Resliced volume saved successfully.")
+            self.request_gui(plt.close, "all")
 
         else:
             self.prompt_message("No file is selected.")
--- plugins/onlypores/plugin.py
+++ plugins/onlypores/plugin.py
@@ -4,8 +4,8 @@ from PyQt6.QtWidgets import *
 from PyQt6.QtCore import *
 import matplotlib.pyplot as plt
 import tifffile as tiff
-import plugins.Onlypores.onlypores as op
 import numpy as np
+from preprocess_tools import reslicer, aligner, onlypores
 
 # Define a class that implements the PluginInterface
 class Plugin(BasePlugin):
@@ -32,15 +32,26 @@ class Plugin(BasePlugin):
             if file_path:
 
                 # Create a progress window
-                self.update_progress(25, "Loading "+ file_path, index, len(files_paths))
+                self.update_progress(0, "Loading "+ file_path, index, len(files_paths))
                 # Load the 3D volume from a .tif file
                 volume = tiff.imread(file_path)
+                #crop frontwal and backwall
+                self.update_progress(25, "Cropping "+ file_path, index, len(files_paths))
+                #axes of volume are (z, y, x)
+                #transform the volume to (x, y, z)
+                volume = np.transpose(volume, (2, 1, 0))
+                #axes of volume are (x, y, z)
+                #crop the volume to remove the walls
+                volume = aligner.crop_walls(volume)
+                #axes of cropped_volume are (x, y, z)
+                #transform the volume to (z, y, x)
+                volume = np.transpose(volume, (2, 1, 0))
                 # Apply the onlypores function to the volume
                 self.update_progress(50, "Calculating "+ file_path, index, len(files_paths))
-                onlypores, sample_mask, binary = op.onlypores(volume)
+                onlypores_volume, sample_mask, binary = onlypores.onlypores(volume)
                 self.update_progress(75, "Saving "+ file_path, index, len(files_paths))
                 save_path = file_path.replace(".tif", "_onlypores.tif")
-                tiff.imsave(save_path, onlypores.astype(np.uint8) * 255)
+                tiff.imsave(save_path, onlypores_volume.astype(np.uint8) * 255)
 
                 # Save sample_mask and binary if requested
                 if save_masks:
